ecec79103a8c41a1d89816f2e1f16889c72b27d4
Owen Craston <owen.craston@shopify.com>
2019-04-09 19:44:54 -0400

    document the password confirmation

--- src/components/UserPasswordConfirmations/PasswordConfirmation/index.js
+++ src/components/UserPasswordConfirmations/PasswordConfirmation/index.js
@@ -1,1 +1,4 @@
+/*
+this file exports the password confirmation component and saves the js import from specifying the specific file
+*/
 export { default as PasswordConfirmation } from "./PasswordConfirmation";
